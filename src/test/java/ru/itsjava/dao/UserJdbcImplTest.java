4c586596b54f27d775dd9d1716e759f4e35f5d80
George Chanturiya <57139729+megrel13@users.noreply.github.com>
2020-10-31 01:28:44 +0300

    add new Test for User.But, there're some issues about parameters in new user. Idea ask create new constructor, but I'm already have lombok's @AllArgsConstructor in domain class User.

--- src/test/java/ru/itsjava/dao/UserJdbcImplTest.java
+++ src/test/java/ru/itsjava/dao/UserJdbcImplTest.java
@@ -1,10 +1,14 @@
 package ru.itsjava.dao;
 
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.context.annotation.Import;
+import ru.itsjava.domain.Email;
+import ru.itsjava.domain.User;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -17,7 +21,34 @@ public class UserJdbcImplTest {
 
     @Test
     @DisplayName(" находить количетсво пользователей")
-    public void shouldCorrectCount(){
-        assertEquals(userDaoJdbc.count(), 1);
+    public void shouldCorrectCount() {
+        assertEquals(userDaoJdbc.count(), 4);
     }
+
+    @Test
+    @DisplayName("Находить по ID")
+    public void shouldCorrectFindById() {
+        User user = new User(3, "ALIEDORA", 33, (),3);
+        Assertions.assertEquals(userDaoJdbc.findById(3), user);
+    }
+
+    @Test
+    @DisplayName("Корректная работа метода insert")
+    public void shouldCorrectInsert() {
+        User user = new User(5, "Kishiki", 33,5,"");
+        userDaoJdbc.insert(user);
+        Assertions.assertEquals(userDaoJdbc.findById(5),user);
+
+    }
+
+    @Test
+    @DisplayName("Корректная работа метода delete")
+    public void shouldCorrectDelete() {
+        User user = new User(2, "Alita",23, 2, 2);
+        userDaoJdbc.deleteUser(user);
+        Assertions.assertEquals(userDaoJdbc.count(), 1);
+    }
+
+
+
 }
