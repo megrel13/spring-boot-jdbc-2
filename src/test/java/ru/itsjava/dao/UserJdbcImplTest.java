9b878f6ecb5312876e50ba8b6803b3beefa4a657
George Chanturiya <57139729+megrel13@users.noreply.github.com>
2020-10-31 22:11:49 +0300

    add tests for UserJdbc class. count and delete are working, FindById and Insert - not

--- src/test/java/ru/itsjava/dao/UserJdbcImplTest.java
+++ src/test/java/ru/itsjava/dao/UserJdbcImplTest.java
@@ -8,7 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.context.annotation.Import;
 import ru.itsjava.domain.Email;
+import ru.itsjava.domain.Pet;
 import ru.itsjava.domain.User;
+
+
+import java.util.Collections;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -22,33 +27,40 @@ public class UserJdbcImplTest {
     @Test
     @DisplayName(" находить количетсво пользователей")
     public void shouldCorrectCount() {
-        assertEquals(userDaoJdbc.count(), 4);
+        assertEquals(userDaoJdbc.count(), 1);
     }
 
     @Test
     @DisplayName("Находить по ID")
     public void shouldCorrectFindById() {
-        User user = new User(3, "ALIEDORA", 33, (),3);
+        Email email = new Email(3, "Aliedora@mail.ru");
+        Pet pet = new Pet(3, "bobik");
+        //      List<> newEmail = Collections.singletonList();
+        User user = new User(3, "ALIEDORA", 33, email, pet);
+        userDaoJdbc.insert(user);
         Assertions.assertEquals(userDaoJdbc.findById(3), user);
     }
 
     @Test
     @DisplayName("Корректная работа метода insert")
     public void shouldCorrectInsert() {
-        User user = new User(5, "Kishiki", 33,5,"");
+        Email email = new Email(5, "MomoMegaOtsutsuki@mail.ru");
+        Pet pet = new Pet(5, "Kinshiki");
+        User user = new User(5, "Momoshiki", 33, email, pet);
         userDaoJdbc.insert(user);
-        Assertions.assertEquals(userDaoJdbc.findById(5),user);
+        Assertions.assertEquals(userDaoJdbc.findById(5), user);
 
     }
 
     @Test
     @DisplayName("Корректная работа метода delete")
     public void shouldCorrectDelete() {
-        User user = new User(2, "Alita",23, 2, 2);
+        Email email = new Email(2, "Alita@mail.ru");
+        Pet pet = new Pet(2, "shaniya");
+        User user = new User(2, "Alita", 23, email, pet);
         userDaoJdbc.deleteUser(user);
         Assertions.assertEquals(userDaoJdbc.count(), 1);
     }
-
 
 
 }
