d6e6bbe5b3c03346b8669f22ee82b3a37f67e56a
George Chanturiya <57139729+megrel13@users.noreply.github.com>
2020-11-01 09:12:15 +0300

    problem with optional - done. But there is a new problem with test method insert.

--- src/test/java/ru/itsjava/dao/UserJdbcImplTest.java
+++ src/test/java/ru/itsjava/dao/UserJdbcImplTest.java
@@ -14,6 +14,7 @@ import ru.itsjava.domain.User;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -27,7 +28,7 @@ public class UserJdbcImplTest {
     @Test
     @DisplayName(" находить количетсво пользователей")
     public void shouldCorrectCount() {
-        assertEquals(userDaoJdbc.count(), 1);
+        assertEquals(userDaoJdbc.count(), 4);
     }
 
     @Test
@@ -37,18 +38,18 @@ public class UserJdbcImplTest {
         Pet pet = new Pet(3, "bobik");
         //      List<> newEmail = Collections.singletonList();
         User user = new User(3, "ALIEDORA", 33, email, pet);
-        userDaoJdbc.insert(user);
-        Assertions.assertEquals(userDaoJdbc.findById(3), user);
+        //userDaoJdbc.insert(user);
+        Assertions.assertEquals(userDaoJdbc.findById(3), Optional.of(user));
     }
 
     @Test
     @DisplayName("Корректная работа метода insert")
     public void shouldCorrectInsert() {
-        Email email = new Email(5, "MomoMegaOtsutsuki@mail.ru");
-        Pet pet = new Pet(5, "Kinshiki");
-        User user = new User(5, "Momoshiki", 33, email, pet);
+        Email email = new Email(4, "dasha@yandex.ru");
+        Pet pet = new Pet(4, "princeGregory");
+        User user = new User(4, "DASHA", 18, email, pet);
         userDaoJdbc.insert(user);
-        Assertions.assertEquals(userDaoJdbc.findById(5), user);
+        Assertions.assertEquals(userDaoJdbc.findById(4), Optional.of(user));
 
     }
 
@@ -59,7 +60,7 @@ public class UserJdbcImplTest {
         Pet pet = new Pet(2, "shaniya");
         User user = new User(2, "Alita", 23, email, pet);
         userDaoJdbc.deleteUser(user);
-        Assertions.assertEquals(userDaoJdbc.count(), 1);
+        Assertions.assertEquals(userDaoJdbc.count(), 4);
     }
 
 
--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,2 +1,18 @@
-INSERT INTO USERS(ID, NAME, AGE)
-VALUES (1, 'MASHA', 10);
+INSERT INTO EMAILS(ID, EMAIL)
+VALUES (1, 'masha@mail.ru'),
+       (2, 'alita@mail.ru'),
+       (3, 'Aliedora@mail.ru'),
+       (4, 'dasha@yandex.ru');
+
+
+INSERT INTO USERS(ID, NAME, AGE, EMAIL_ID)
+VALUES (1, 'MASHA', 20, 1),
+       (2, 'ALITA', 23, 2),
+       (3, 'ALIEDORA', 33, 3),
+       (4, 'DASHA', 18, 4);
+
+INSERT INTO PETS(ID, NICKNAME, USER_ID)
+VALUES (1, 'barsik', 1),
+       (2, 'shaniya', 2),
+       (3, 'bobik', 3),
+       (4, 'princeGregory', 4);
